bd66732c4c5e373a3f5b411e2fe107c9da43c774
Weekend-Dracon <102872724+Weekend-Dracon@users.noreply.github.com>
2023-04-20 14:01:48 +0500

    added templates and static files

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+
 
 # Create your views here.
+
+def home_view(request):
+    return render(request, "pages/index.html")
+
+
+def shop_view(request):
+    return HttpResponse("Страница категорий")
+
+
+def product_detail(request, slug):
+    return HttpResponse("Страница продукта")
